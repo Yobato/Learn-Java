c791e03a04b8079fc64ec7b29986bdee3efec7fa
Yobato <satriyobagus30077@gmail.com>
2025-03-21 10:42:14 +0700

    init:learn java data type

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        String name = "BSI";
+        int x = 5;
+        int y = 10;
+        x=9;
+        int sum = x + y;
+        System.out.println("Hello, " + name);
+        System.out.println("The sum of " + x + " and " + y + " is " + sum);
+    }
+}
